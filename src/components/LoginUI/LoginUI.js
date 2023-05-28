e56fe848c7291439e30b3e010f8060d216826cae
Hariharasuthan B <hari111.balaji@gmail.com>
2023-05-28 11:38:34 +0530

    Refactor on authentication (#4)
    * Revert "Revert "Revert "fix: used hash router to solve netlify route issue"""
    
    This reverts commit 2599f530d0fe2612e7efc3be87bf377cee848e9a.
    
    * refactor: used usenavigate for redirects

--- src/components/LoginUI/LoginUI.js
+++ src/components/LoginUI/LoginUI.js
@@ -1,40 +1,44 @@
 import firebase from "firebase/compat/app";
 import * as firebaseui from "firebaseui";
 import "firebaseui/dist/firebaseui.css";
-import { useEffect } from "react";
-const { location, localStorage } = window;
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
+import { isLoggedIn } from "../../utils/loginUtil";
+
+const { localStorage } = window;
+const uiConfig = {
+  signInFlow: "popup",
+  signInOptions: [firebase.auth.GoogleAuthProvider.PROVIDER_ID],
+};
 const setupSigninUI = (apiKey) => {
   var config = {
     apiKey,
     authDomain: "hb-todo-react.firebaseapp.com",
   };
   firebase.initializeApp(config);
-  var uiConfig = {
-    callbacks: {
-      signInSuccessWithAuthResult: (authResult, redirectUrl) => {
-        console.log({ authResult });
-        location.hash = "/items";
-        localStorage.setItem("uid", authResult.user.uid);
-      },
-    },
-    signInFlow: "popup",
-
-    signInOptions: [firebase.auth.GoogleAuthProvider.PROVIDER_ID],
-  };
-
   var ui = new firebaseui.auth.AuthUI(firebase.auth());
-  ui.start("#loginUI", uiConfig);
+  return ui;
 };
+const FBUI = setupSigninUI(process.env.REACT_APP_FIREBASE_API_KEY);
+const provider = new GoogleAuthProvider();
 
 const LoginUI = () => {
-  let loggedIn = localStorage.getItem("uid");
-  debugger;
-  useEffect(() => {
-    if (!loggedIn) {
-      setupSigninUI(process.env.REACT_APP_FIREBASE_API_KEY);
+  const navigate = useNavigate();
+  const runLoginUI = async () => {
+    if (!isLoggedIn()) {
+      FBUI.start("#loginUI", uiConfig);
+      const auth = getAuth();
+      let result = await signInWithPopup(auth, provider);
+      const user = result.user;
+      localStorage.setItem("uid", user.uid);
+      navigate("/items");
     }
-  }, [loggedIn]);
-  return <div id="loginUI"></div>;
+  };
+  return (
+    <div id="loginUI">
+      <button onClick={runLoginUI}> Sign in with google</button>
+    </div>
+  );
 };
 
 export default LoginUI;
