49f2bb2021807ab4e64eed74c10ea747433a5b2d
Hariharasuthan B <hari111.balaji@gmail.com>
2023-08-23 14:59:21 +0530

    chore: top bar minor changes

--- src/components/TopBar/TopBar.js
+++ src/components/TopBar/TopBar.js
@@ -1,12 +1,14 @@
 import { Heading, Separator } from "@radix-ui/themes";
 import styles from "./TopBar.module.css";
 import LogoutUI from "../LogoutUI/LogoutUI";
+import { MixIcon } from "@radix-ui/react-icons";
 const TopBar = (props) => {
   return (
     <div>
       <div className={styles.TopBar}>
+        <MixIcon width="25" height="25"></MixIcon>
         <Heading size="6"> To do</Heading>
-        {props.userLoggedIn && <LogoutUI></LogoutUI>}
+        {props.userLoggedIn ? <LogoutUI></LogoutUI> : "Not logged in"}
       </div>
       <Separator my="3" size="4" />
     </div>
