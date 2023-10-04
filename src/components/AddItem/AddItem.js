4c715e9441b654f183006686dbee4ed0bdb4aca1
Hariharasuthan B <hari111.balaji@gmail.com>
2023-10-04 15:27:58 +0530

    chore: enforced max limit on tasks

--- src/components/AddItem/AddItem.js
+++ src/components/AddItem/AddItem.js
@@ -1,19 +1,26 @@
 import { useState } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { addItemReq } from "../../store/itemsSlice";
 import { Button, TextField } from "@radix-ui/themes";
 import { PlusIcon } from "@radix-ui/react-icons";
 import styles from "./AddItem.module.css";
 
 const AddItem = () => {
+  const MAX_LIMIT = 20;
   const [inputValue, setInputValue] = useState("");
   const disableInputValue = !inputValue.length;
   const dispatch = useDispatch();
   const handleInputChange = (e) => {
     setInputValue(e.target.value);
   };
+  const items = useSelector((state) => {
+    return state.todoListItems.items;
+  });
 
   const handleAddItem = () => {
+    if (items.length >= MAX_LIMIT) {
+      alert(`You can't add more than ${MAX_LIMIT} tasks`);
+    }
     let item = {
       name: inputValue,
       isCompleted: false,
