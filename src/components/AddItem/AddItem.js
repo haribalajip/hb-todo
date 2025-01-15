35da627b1b19e685480ed051acb864dbacfc772b
Hariharasuthan B <hari111.balaji@gmail.com>
2025-01-15 12:28:30 +0530

    chore: limit reached callout added

--- src/components/AddItem/AddItem.js
+++ src/components/AddItem/AddItem.js
@@ -1,15 +1,14 @@
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { addItemReq } from "../../store/itemsSlice";
-import { Button, TextField } from "@radix-ui/themes";
-import { PlusIcon } from "@radix-ui/react-icons";
+import { Button, TextField, Callout } from "@radix-ui/themes";
+import { PlusIcon, InfoCircledIcon } from "@radix-ui/react-icons";
 import Spinner from "../Spinner/Spinner";
 import styles from "./AddItem.module.css";
 
 const AddItem = () => {
   const MAX_LIMIT = 20;
   const [inputValue, setInputValue] = useState("");
-  const disableInputValue = !inputValue.length;
   const dispatch = useDispatch();
   const handleInputChange = (e) => {
     setInputValue(e.target.value);
@@ -17,11 +16,11 @@ const AddItem = () => {
   const items = useSelector((state) => {
     return state.todoListItems.items;
   });
+  
+  const limitReached = items.length >= MAX_LIMIT;
+  const disableInputValue = !inputValue.length || limitReached;
 
   const handleAddItem = () => {
-    if (items.length >= MAX_LIMIT) {
-      alert(`You can't add more than ${MAX_LIMIT} tasks`);
-    }
     let item = {
       name: inputValue,
       isCompleted: false,
@@ -36,27 +35,41 @@ const AddItem = () => {
   );
 
   return (
-    <form className={styles.AddItem} onSubmit={handleAddItem}>
-      <TextField.Input
-        value={inputValue}
-        placeholder="Pop in the task name"
-        onChange={handleInputChange}
-      ></TextField.Input>
+    <div>
+      <form className={styles.AddItem} onSubmit={handleAddItem}>
+        <TextField.Input
+          value={inputValue}
+          placeholder="Pop in the task name"
+          onChange={handleInputChange}
+        ></TextField.Input>
 
-      <Button
-        type="submit"
-        onClick={handleAddItem}
-        disabled={disableInputValue}
-      >
-        {isItemBeingAdded ? (
-          <Spinner customClassName="svg-sm" />
-        ) : (
-          <>
-            <PlusIcon /> Add task
-          </>
-        )}
-      </Button>
-    </form>
+        <Button
+          type="submit"
+          onClick={handleAddItem}
+          disabled={disableInputValue}
+        >
+          {isItemBeingAdded ? (
+            <Spinner customClassName="svg-sm" />
+          ) : (
+            <>
+              <PlusIcon /> Add task
+            </>
+          )}
+        </Button>
+      </form>
+      {(limitReached) &&
+        <div className={styles.calloutContainer}>
+          <Callout.Root size="1">
+            <Callout.Icon>
+              <InfoCircledIcon />
+            </Callout.Icon>
+            <Callout.Text>
+              You've reached the maximum limit of tasks. Try deleting a few to add further.
+            </Callout.Text>
+          </Callout.Root>
+        </div>
+      }
+    </div>
   );
 };
 export default AddItem;
