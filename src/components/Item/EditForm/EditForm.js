d84df3d8317ae059c6b79518ddc568e0de0a77b5
Hariharasuthan B <hari111.balaji@gmail.com>
2025-03-16 16:29:02 +0530

    chore: style fixes, date hidden for all

--- src/components/Item/EditForm/EditForm.js
+++ src/components/Item/EditForm/EditForm.js
@@ -44,11 +44,11 @@ const EditForm = ({ item, saveItem }) => {
 				</fieldset>
 			</div>
 			<div className={styles.footer}>
-				<Button variant="soft" color="cyan" onClick={() => setOpen(false)}>Cancel</Button>
+				<Button variant="soft" color="cyan" className="mg-r-10" onClick={() => setOpen(false)}>Cancel</Button>
 				{isSaving ? (
 					<Spinner customClassName={`svg-sm`} />
 				) : (
-					<Button className="mg-l-10" variant='soft' type="submit" onClick={onSave}>Save</Button>
+					<Button variant='soft' type="submit" onClick={onSave}>Save</Button>
 				)}
 			</div>
 		</>
